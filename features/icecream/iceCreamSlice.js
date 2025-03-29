78fa79de14b28f57ad09d6eb921eed72a30fb56e
Abhijeet.maity <Abhijeet.maity@nimapinfotech.in>
2025-03-29 17:30:17 +0530

    added extra reducers

--- features/icecream/iceCreamSlice.js
+++ features/icecream/iceCreamSlice.js
@@ -1,21 +1,53 @@
+// const createSlice = require("@reduxjs/toolkit").createSlice;
+
+// const initialState = {
+//     numOfIcecreams: 20,
+// }
+
+// const iceCreamSlice = createSlice({
+//     name: 'icecream',
+//     initialState,
+//     reducers: {
+//         ordered: state => {
+//             state.numOfIcecreams--
+//         },
+//         restocked: (state, action) => {
+//             state.numOfIcecreams += action.payload
+//         },
+//     },
+// })
+
+// module.exports = iceCreamSlice.reducer;
+// module.exports.iceCreamActions = iceCreamSlice.actions;
+
 const createSlice = require("@reduxjs/toolkit").createSlice;
 
 const initialState = {
-    numOfIcecreams: 20,
-}
+  numOfIcecreams: 10,
+};
 
 const iceCreamSlice = createSlice({
-    name: 'icecream',
-    initialState,
-    reducers: {
-        ordered: state => {
-            state.numOfIcecreams--
-        },
-        restocked: (state, action) => {
-            state.numOfIcecreams += action.payload
-        },
+  name: "iceCream",
+  initialState,
+  reducers: {
+    ordered: (state = initialState) => {
+      state.numOfIcecreams--;
     },
-})
+    restocked: (state, action) => {
+      state.numOfIcecreams += action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase("cake/ordered", (state) => {
+      state.numOfIcecreams--;
+    });
+  },
+  // extraReducers: {
+  //     ['cake/ordered']:(state) => {
+  //         state.numOfIcecreams--;
+  //     },
+  // },
+});
 
 module.exports = iceCreamSlice.reducer;
 module.exports.iceCreamActions = iceCreamSlice.actions;
--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const store  = require("./app/store");
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
 const iceCreamActions = require('./features/icecream/iceCreamSlice').iceCreamActions
+const fetchUsers = require('./features/user/userSlice').fetchUsers
 
 console.log('Initial state', store.getState());
 const unsubscribe = store.subscribe(() => {
     console.log('Updated state', store.getState())
 })
 
+store.dispatch(fetchUsers());
+
 store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
 
 
-store.dispatch(iceCreamActions.ordered())
-store.dispatch(iceCreamActions.ordered())
-store.dispatch(iceCreamActions.ordered(2))
+// store.dispatch(iceCreamActions.ordered())
+// store.dispatch(iceCreamActions.ordered())
+// store.dispatch(iceCreamActions.ordered())
 
-unsubscribe();
+// unsubscribe();
